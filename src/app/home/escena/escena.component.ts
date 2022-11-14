96f743e9dc4ebd512706cb7cb773d52468e9ba41
Nacho1886 <francert@hotmail.com>
2022-11-14 20:48:18 +0100

    Paint bugs

--- src/app/home/escena/escena.component.ts
+++ src/app/home/escena/escena.component.ts
@@ -11,9 +11,22 @@ import { Sentence } from "../interfaces/Sentence";
 })
 export class EscenaComponent {
   
-  // this.sentences =
+  currentIndexSentence: number = 0
+
+  currentSentence: string = this.sentences[this.currentIndexSentence].txt
+
   get sentences(): Sentence[] {return this.sentencesService.showSentences}
 
+  next(): void {
+    this.currentIndexSentence++
+    this.sentences[this.currentIndexSentence]
+  }
+  prev(): void {this.currentIndexSentence--}
+  
+  activeSentence(current: number): void {
+    this.currentIndexSentence = current;
+    this.sentences[this.currentIndexSentence]
+  }
 
   constructor(
     private sentencesService: SentencesService
