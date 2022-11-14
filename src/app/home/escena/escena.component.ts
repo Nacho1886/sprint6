8a895a151fb7a02784a0048d53acdb96ea7292a0
Nacho1886 <francert@hotmail.com>
2022-11-14 23:50:00 +0100

    Lvl 1 finished

--- src/app/home/escena/escena.component.ts
+++ src/app/home/escena/escena.component.ts
@@ -13,19 +13,20 @@ export class EscenaComponent {
   
   currentIndexSentence: number = 0
 
-  currentSentence: string = this.sentences[this.currentIndexSentence].txt
-
   get sentences(): Sentence[] {return this.sentencesService.showSentences}
 
   next(): void {
     this.currentIndexSentence++
-    this.sentences[this.currentIndexSentence]
+    if (this.currentIndexSentence === 4) this.currentIndexSentence = 0
   }
-  prev(): void {this.currentIndexSentence--}
   
-  activeSentence(current: number): void {
-    this.currentIndexSentence = current;
-    this.sentences[this.currentIndexSentence]
+  prev(): void {
+    this.currentIndexSentence--
+    if (this.currentIndexSentence === -1) this.currentIndexSentence = 3
+  }
+  
+  activeSentence(): Sentence {
+    return this.sentences[this.currentIndexSentence];
   }
 
   constructor(
